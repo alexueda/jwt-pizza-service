33e1cd5f764a8887c19f956409f9add2c330f424
alexueda <alex402403@gmail.com>
2025-02-03 10:48:32 -0700

    list solve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-/ @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: { ...globals.jest, ...globals.node } } },
   pluginJs.configs.recommended,
 ];
--- src/service.test.js
+++ src/service.test.js
@@ -4,19 +4,17 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
+  await request(app).post('/api/auth').send(testUser);
 });
 
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-
+  // eslint-disable-next-line no-unused-vars
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
 });
